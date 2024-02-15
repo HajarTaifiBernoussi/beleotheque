b77a7666109d97a29df02c794b5cbdde30778ca3
HP LAPTOP <HP LAPTOP@hajar-Taifi-Bernoussi>
2024-02-15 17:24:33 +0100

    initial commit.

---
+++ beblioth/src/beblioth/book.java
@@ -0,0 +1,19 @@
+public class book{
+
+    
+    private String titre;
+    private String auteur;
+    private int anneePublication;
+
+    public Livre(String titre, String auteur, int anneePublication) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.anneePublication = anneePublication;
+    }
+    
+    public void afficherDetails() {
+        System.out.println("Titre : " + titre);
+        System.out.println("Auteur : " + auteur);
+        System.out.println("Année de publication : " + anneePublication);
+    }
+}
---
+++ beblioth/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HP LAPTOP
+ *
+ */
+module beblioth {
+}
